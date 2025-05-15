ef71044b714298e21e41ca86db7949d818382776
Łukasz Lech <lesiu@hot.pl>
2025-05-15 19:34:18 +0200

    Add project files.

---
+++ UstawianieKuriera/UstawianieKuriera/ProgramInit.cs
@@ -0,0 +1,26 @@
+﻿using Soneta.Business;
+using Soneta.Business.App;
+using UstawianieKuriera;
+
+[assembly: ProgramInitializer(typeof(ProgramInit))]
+[assembly: Service(typeof(ILoginListener), typeof(ProgramInit), ServiceScope.Login)]
+namespace UstawianieKuriera;
+
+class ProgramInit : ILoginListener, IProgramInitializer
+{
+  static ProgramInit() {
+    Soneta.Handel.HandelModule.DokumentHandlowySchema
+      //taka dziwaczna z pozoru konstrukcja odczytuje wartość cechy
+      //po czym przypisuje ją do niej ponownie czym wywołuje cały kod
+      //na niej zdefiniowany do weryfikacji poprawności,
+      //który zadziała już na nowowybranym kontrahencie. 
+      .AddKontrahentAfterEdit(row => { 
+        row.Features["Kurier"] = row.Features["Kurier"]; });
+  }
+
+  public void Initialize() { }
+  public void BeforeLogin(Session session) { }
+  public void AfterLogin(Login login) { }
+  public void BeforeUnlogin(Login login) { }  
+}
+
---
+++ UstawianieKuriera/UstawianieKuriera/RaportKurierow.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Soneta.Business;
+using Soneta.CRM;
+using Soneta.Handel;
+using Soneta.Types;
+using UstawianieKuriera;
+
+[assembly: Worker(typeof(RaportKurierow),typeof(DokHandlowe))]
+namespace UstawianieKuriera;
+
+public class RaportKurierow
+{
+  [Context] public DokumentHandlowy[] Dokumenty { get; set; }
+
+
+  [Action("Generuj raport",
+    Mode = ActionMode.SingleSession,
+    Target = ActionTarget.ToolbarWithText)]  
+  public object UtworzRaport(Context cx) {
+    if(!Dokumenty?.Any() ?? false) return "Zaznacz faktury.";
+
+    var raport = Dokumenty!.Select(d => d.Features.GetString("Kurier"))
+      .Where(k=>!string.IsNullOrWhiteSpace(k))
+      .GroupBy(k => k)
+      .Select(k => new { kurier = k.Key,liczba = k.Count() });
+
+    var nazwaRaportu = Date.Today.ToString("yyyy-MM-dd")
+      + " - Raport kurierów ";
+    var raportTxt = nazwaRaportu + Environment.NewLine
+      + string.Join(Environment.NewLine,
+        raport.Select(k => k.kurier + ": " + k.liczba).ToArray());
+
+#if DEBUG
+    new Log("DebugInfo",true).WriteLine(raportTxt);
+#endif
+
+    return new NamedStream(nazwaRaportu+".txt",
+      Encoding.UTF8.GetBytes(raportTxt));
+  }
+}
